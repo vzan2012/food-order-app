e23e6436a3e08eb4bd2863ebf0fb16812a43c522
Deepak Guptha <vzan2012@gmail.com>
2023-04-28 00:00:55 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  // content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
