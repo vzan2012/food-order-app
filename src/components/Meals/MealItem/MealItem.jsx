1ca27db9a01569e0aa2e937ea9b63e428300e8d4
Deepak Guptha <vzan2012@gmail.com>
2023-04-30 16:17:18 +0530

    feat: updated input component for the MealItemForm component

--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -1,16 +1,21 @@
 import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
 
 const MealItem = ({ id, name, description, price }) => {
   const mealPrice = `$ ${price.toFixed(2)}`;
 
   return (
     <li key={id}>
+      {/* Meal Item - Properties  */}
       <div className={classes.meal}>
         <h3>{name}</h3>
         <div className={classes.description}>{description}</div>
         <div className={classes.price}>{mealPrice}</div>
       </div>
-      <div></div>
+      {/* Meal Item - Form  */}
+      <div>
+        <MealItemForm />
+      </div>
     </li>
   );
 };
