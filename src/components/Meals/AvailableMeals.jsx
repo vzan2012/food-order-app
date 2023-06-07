aee414ea98bc12919d38b471cd88d5f6350da9d6
Deepak Guptha <vzan2012@gmail.com>
2023-06-07 17:42:03 +0530

    feat: http error handling

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -4,32 +4,44 @@ import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import { useEffect, useState } from "react";
 
+// Fe
 const mealsURL = `${import.meta.env.VITE_FIREBASE_URL}/meals.json`;
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
 
   const fetchMealsData = async (mealsURL) => {
-    const response = await fetch(mealsURL);
-    const mealsData = await response.json();
+    try {
+      const response = await fetch(mealsURL);
 
-    const loadedMeals = [];
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
 
-    for (const mealKey in mealsData) {
-      const food = {
-        id: mealKey,
-        name: mealsData[mealKey].name,
-        description: mealsData[mealKey].description,
-        price: mealsData[mealKey].price,
-      };
+      const mealsData = await response.json();
 
-      loadedMeals.push(food);
+      const loadedMeals = [];
+
+      for (const mealKey in mealsData) {
+        const food = {
+          id: mealKey,
+          name: mealsData[mealKey].name,
+          description: mealsData[mealKey].description,
+          price: mealsData[mealKey].price,
+        };
+
+        loadedMeals.push(food);
+      }
+
+      setMeals(loadedMeals);
+
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      setHttpError(error.message);
     }
-
-    setMeals(loadedMeals);
-
-    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -40,6 +52,14 @@ const AvailableMeals = () => {
     return (
       <section className={classes.mealsloading}>
         <p>Loading ...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.mealserror}>
+        <p>{httpError}</p>
       </section>
     );
   }
